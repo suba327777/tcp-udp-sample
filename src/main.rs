a0e3efb662acf9d0706e58da9d5da2ed3c214b8a
suba <84484832+suba327777@users.noreply.github.com>
2023-05-27 14:49:39 +0900

    add: tcp echo server

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use log::*;
+use std::env;
+mod tcp_server;
+fn main() {
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    //プログラムのコマンドライン引数を取得
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        error!("Please specify [tcp|udp] [server|client] [addr:port]");
+        std::process::exit(1);
+    }
+
+    let protocol: &str = &args[1];
+    let role: &str = &args[2];
+    let address = &args[3];
+
+    match protocol {
+        "tcp" => match role {
+            "server" => {
+                //TCPサーバ呼び出し
+                tcp_server::serve(address).unwrap_or_else(|e| error!("{}", e));
+            }
+            "client" => {
+                //TCPクライアンド呼び出し
+            }
+            _ => {
+                missing_role();
+            }
+        },
+        "udp" => match role {
+            "server" => {
+                //UDPサーバ呼び出し
+            }
+            "client" => {
+                //UDPクライアント呼び出し
+            }
+            _ => {
+                missing_role();
+            }
+        },
+        _ => {
+            error!("Please specifty tcp or udp on the 1st argument. ");
+            std::process::exit(1);
+        }
+    }
+}
+
+//第二引数が不正な時にエラーを出す関数
+fn missing_role() {
+    error!("Please specify server or client on the 2nd argument");
+    std::process::exit(1);
+}
---
+++ src/tcp_server.rs
@@ -0,0 +1,36 @@
+use log::*;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::{str, thread};
+
+//指定のソケットアドレスで接続を待ち受ける
+pub fn serve(address: &str) -> Result<(), failure::Error> {
+    let listener = TcpListener::bind(address)?;
+    loop {
+        let (stream, _) = listener.accept()?;
+
+        //スレッドを立ち上げて接続に対処する
+        thread::spawn(move || {
+            handler(stream).unwrap_or_else(|error| error!("{:?}", error));
+        });
+    }
+}
+
+//クライアントからの入力を待ち受け、受信したら同じものを返却する
+fn handler(mut stream: TcpStream) -> Result<(), failure::Error> {
+    //接続元のアドレスを取得
+    debug!("handling data from {}", stream.peer_addr()?);
+    let mut buffer = [0u8; 1024];
+    loop {
+        let nbytes = stream.read(&mut buffer)?;
+        //クライアントの接続が切れたら終了
+        if nbytes == 0 {
+            dbg!("connection closed.");
+            return Ok(());
+        }
+        print!("{}", str::from_utf8(&buffer[..nbytes])?);
+        //受信データを返却
+        stream.write_all(&buffer[..nbytes])?;
+    }
+}
+
