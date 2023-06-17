03b2ad9894642d3e46a2fb357a9c06c23589224e
suba <84484832+suba327777@users.noreply.github.com>
2023-06-17 12:48:16 +0900

    add: bits_per_second

--- src/tcp_server.rs
+++ src/tcp_server.rs
@@ -1,6 +1,8 @@
 use log::*;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::time::Instant;
 use std::{str, thread};
 
 //指定のソケットアドレスで接続を待ち受ける
@@ -24,8 +26,13 @@ fn handler(mut stream: TcpStream) -> Result<(), failure::Error> {
     //接続元のアドレスを取得
     debug!("handling data from {}", stream.peer_addr()?);
     let mut buffer = [0u8; 1024];
+    let mut total_bytes = 0;
+    let mut total_elapsed_time = Duration::default();
     loop {
+        let start_time = Instant::now();
         let nbytes = stream.read(&mut buffer)?;
+        total_bytes += nbytes;
+
         //クライアントの接続が切れたら終了
         if nbytes == 0 {
             dbg!("connection closed.");
@@ -34,5 +41,12 @@ fn handler(mut stream: TcpStream) -> Result<(), failure::Error> {
         print!("{}", str::from_utf8(&buffer[..nbytes])?);
         //受信データを返却
         stream.write_all(&buffer[..nbytes])?;
+        let end_time = Instant::now();
+        let elapsed_time = end_time - start_time;
+        total_elapsed_time += elapsed_time;
+
+        //受信速度
+        let bits_per_second = (total_bytes as f64 * 8.0) / total_elapsed_time.as_secs_f64();
+        println!(" {:?} bits/s", bits_per_second);
     }
 }
